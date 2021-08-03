fa6c8a3f83d5408b13bf16e6516ee9e9ec1e4a22
Alberto Gonzalez <78628287+dalismo@users.noreply.github.com>
2021-08-03 14:34:20 -0400

    still working on structure

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -1,2 +1,10 @@
-let geo_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// Defining earthquake plates using GeoJSON
+let geo_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 console.log(geo_quakes)
+
+// Create the earthquake layergroup
+let earthquake = L.layergroup();
+
+// Creating the tile layer
+
+
