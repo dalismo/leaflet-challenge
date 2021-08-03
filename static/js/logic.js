4610820f6d526b302a85590f6a3778e395f75e58
Alberto Gonzalez <78628287+dalismo@users.noreply.github.com>
2021-08-03 16:05:24 -0400

    working on repo

---
+++ static/js/logic.js
@@ -0,0 +1,10 @@
+// Defining earthquake plates using GeoJSON
+let geo_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+console.log(geo_quakes)
+
+// Create the earthquake layergroup
+let earthquake = L.layergroup();
+
+// Creating the tile layer
+
+
