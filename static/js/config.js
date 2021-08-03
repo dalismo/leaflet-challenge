5467e52b2aaa69c7900deb388c635550aa902218
Alberto Gonzalez <78628287+dalismo@users.noreply.github.com>
2021-08-03 16:21:21 -0400

    adding tilelayers

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// My API key
+const API_KEY ="pk.eyJ1IjoiZGFsaXNtbyIsImEiOiJja3J3aTU4amkwZ3B6MnJwYWJ6d2hmNXl1In0.x7Ma4SZgpU-xjMWbtAJv7A";
--- static/js/logic.js
+++ static/js/logic.js
@@ -8,5 +8,10 @@ let earthquakes = L.layergroup();
 let tectonicplates = L.layergroup();
 
 // Creating the tile layers
-
+var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+});
 
